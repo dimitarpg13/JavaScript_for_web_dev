0c56bf8bc77290c0120d780cef4940a2d477ded1
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-08-04 23:51:00 -0400

    update test file

--- src/lang/collections_tests.js
+++ src/lang/collections_tests.js
@@ -3,8 +3,9 @@
 let colors = ["red", "green", "blue"];
 let colors_concatenated = colors.concat("yellow", ["black", "brown"]);
 
-console.log(colors);  // ["red", "green", "blue"]
-console.log(colors_concatenated); // ["red", "green", "blue", "yellow", "black", "brown"]
+console.log("\n--- manipulation methods ---\n")
+console.log('colors =', colors);  // ["red", "green", "blue"]
+console.log('colors_concatenated =' , colors_concatenated); // ["red", "green", "blue", "yellow", "black", "brown"]
 
 // overriding the force-flattening default behavior by specifying a special symbol on the argument array instance
 // Symbol.isConcatSpreadable
@@ -24,16 +25,27 @@ let colors_concatenated2 = colors.concat("yellow", newColors);
 
 let colors_concatenated3 = colors.concat(moreNewColors);
 
-console.log(colors_concatenated2); // ["red", "green", "blue", "yellow", ["black", "brown"]]
+console.log('colors_concatenated2 =', colors_concatenated2); // ["red", "green", "blue", "yellow", ["black", "brown"]]
 
-console.log(colors_concatenated3); // ["red", "green", "blue", "yellow", "black", "brown"]
+console.log('colors_concatenated3 =', colors_concatenated3); // ["red", "green", "blue", "yellow", "black", "brown"]
+
+colors = ["red", "green", "blue", "yellow", "purplse"];
+let colors2 = colors.slice(1);
+let colors3 = colors.slice(1, 4);
+
+console.log('colors2 =', colors2);
+console.log('colors3 =', colors3);
+
+// splice examples
+
 
 // search and location methods
 let person = { name: "Nicholas"};
 let people = [{ name: "Nicholas"}];
 let morePeople = [person];
 
-console.log(people.indexOf(person));      // -1
+console.log("\n---- search and location methods ----\n");
+console.log('people.indexOf(person) =', people.indexOf(person));      // -1
 console.log(morePeople.indexOf(person));  // 0
 console.log(people.includes(person));     // false
 console.log(morePeople.includes(person)); // true

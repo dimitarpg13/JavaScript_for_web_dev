112d1a3b1847a3891c6b57cfa960b4e0e97d96c7
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-08-06 22:48:53 -0400

    add test code

--- src/lang/sample_code/functions.js
+++ src/lang/sample_code/functions.js
@@ -7,3 +7,19 @@ console.log('bar.name: ', bar.name); // bar
 console.log('baz.name: ', baz.name); //baz
 console.log('(() => {}).name: ',(() => {}).name); // (empty string)
 console.log('(new Function()).name: ', (new Function()).name); // anonymous
+
+console.log('foo.bind(null).name: ', foo.bind(null).name); // bound foo
+
+let dog = {
+    years: 1,
+    get age() {
+        return this.years;
+    },
+    set age(newAge) {
+        this.years = newAge;
+    }
+}
+
+let propertyDescriptor = Object.getOwnPropertyDescriptor(dog, 'age');
+console.log('propertyDescriptor.get.name: ', propertyDescriptor.get.name);  // get age
+console.log('propertyDescriptor.set.name: ', propertyDescriptor.set.name);  // set age

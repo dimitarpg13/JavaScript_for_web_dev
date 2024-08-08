e70ff99c1067e5560683931a2a792ab42e3bc49d
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-08-08 11:54:02 -0400

    update sample code

--- src/lang/sample_code/functions.js
+++ src/lang/sample_code/functions.js
@@ -23,3 +23,58 @@ let dog = {
 let propertyDescriptor = Object.getOwnPropertyDescriptor(dog, 'age');
 console.log('propertyDescriptor.get.name: ', propertyDescriptor.get.name);  // get age
 console.log('propertyDescriptor.set.name: ', propertyDescriptor.set.name);  // set age
+
+// arguments in arrow functions
+//
+// When a function is defined using the arrow notation, the arguments passed to the function
+// cannot be accessed using the `arguments` keyword; they can only be accessed using their named
+// token in the function definition.
+
+function foo() {
+    console.log('arguments[0]: ', arguments[0]);
+}
+foo(5); // 5
+
+
+// let bar = () => {
+//   console.log(arguments[0]);   
+// };
+// bar(5); ReferenceError: arguments is not defined
+
+function foo2() {
+    let bar = () => {
+       console.log('arguments[0]: ', arguments[0]);  // 5
+    };
+    bar();
+}
+
+foo2(5);
+
+// nested function definitions
+
+function createComparisonFunction(propertyName) {
+   return function(object1, object2) {
+      let value1 = object1[propertyName];
+      let value2 = object2[propertyName];
+       
+      if (value1 < value2) {
+        return -1;
+      } else if (value1 > value2) {
+        return 1;
+      } else {
+        return 0;
+      }
+   };
+}
+
+let data = [
+    {name: "Zachary", age: 28},
+    {name: "Nicholas", age: 29}
+];
+
+data.sort(createComparisonFunction("name"));
+console.log('data[0].name: ', data[0].name); // Nicholas
+
+data.sort(createComparisonFunction("age"));
+console.log('data[0].name: ', data[0].name); // Zachary
+

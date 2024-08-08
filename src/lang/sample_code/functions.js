82cbaea2a55fa2d05a2321ce7551466a0f751054
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-08-08 12:02:44 -0400

    update sample code

--- src/lang/sample_code/functions.js
+++ src/lang/sample_code/functions.js
@@ -78,3 +78,21 @@ console.log('data[0].name: ', data[0].name); // Nicholas
 data.sort(createComparisonFunction("age"));
 console.log('data[0].name: ', data[0].name); // Zachary
 
+// function arguments
+
+function factorial(num) {
+    if (num <= 1) {
+        return 1;
+    } else {
+        return num * arguments.callee(num - 1);
+    }
+}
+
+let trueFactorial = factorial;
+
+factorial = function() {
+    return 0;
+}
+
+console.log('trueFactorial(5): ', trueFactorial(5)); // 120
+console.log('factorial(5): ', factorial(5));

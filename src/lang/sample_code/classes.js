a572905d56d339fd80e6719abc8da8c82d5f8ab2
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-08-24 16:26:55 -0400

    update classes

--- src/lang/sample_code/classes.js
+++ src/lang/sample_code/classes.js
@@ -133,6 +133,116 @@
     console.log('book.edition: ', book.edition); // 2
 }
 
+{
+    // reading property attributes
+    //
+    // it is possible to retrieve the property descriptor for a given property by using
+    // `Object.getOwnPropertyDescriptor()`. The first argument is the object on which the property descriptor resides
+    // and the second one is the name of the property whose descriptor should be retrieved. 
+    // The return value is an object with properties for `configurable`, `enumerable`, `get`, and `set` for 
+    // accessor properties or `configurable`, `enumerable`, `writable`, and `value` for data properties. 
+    // For example:
+    let book = {};
+    Object.defineProperties(book, {
+        year_: {
+            value: 2017
+        },
+
+        edition: {
+            value: 1
+        },
+
+        year: {
+            get: function() {
+                return this.year_;
+            },
+
+            set: function(newValue) {
+                if (newValue > 2017) {
+                    this.year_ = newValue;
+                    this.edition += newValue - 2017;
+                }
+            }
+        }
+    });
+
+    let descriptor = Object.getOwnPropertyDescriptor(book, "year_");
+    console.log('descriptor.value: ', descriptor.value);        // 2017
+    console.log('decriptor.configurable: ', descriptor.configurable); // false
+    console.log('typeof descriptor.get: ', typeof descriptor.get);   // 'undefined'
+    let descriptor2 = Object.getOwnPropertyDescriptor(book, "year");
+    console.log('descriptor2.value: ', descriptor2.value);  // undefined
+    console.log('descriptor2.enumerable: ', descriptor2.enumerable)  // false
+    console.log('typeof descriptor2.get: ', descriptor2.get); // 'function'
+    
+
+
+}
+
+{
+    // new in ECMAScript 2017: `Object.getOwnPropertyDescriptors()` static method
+    //
+    // 
+    let book = {};
+    Object.defineProperties(book, {
+        year_: {
+            value: 2017
+        },
+
+        edition: {
+            value: 1
+        },
+
+        year: {
+            get: function() {
+                return this.year_;
+            },
+
+            set: function(newValue) {
+                if (newValue > 2017) {
+                    this.year_ = newValue;
+                    this.edition += newValue - 2017;
+                }
+            }
+        }
+    });
+
+    console.log('Object.getOwnPropertyDescriptors(book): ', Object.getOwnPropertyDescriptors(book));
+}
+
+{
+    
+    // Merging objects - referred to as `mixin`
+    // ECMAScript 6 introduces this behavior with the `Object.assign()`.
+    // This method accepts one dest object and one or many sourc objects.
+    // For each source object copies the enumerable (`Object.propertyIsEnumerable` returns `true`)
+    // and own (`Object.hasOwnProperty` returns `true`) properties onto the destination object. 
+    // Properties keyed with strings and symbols will be copied. For each suitable property, the method will 
+    // use `[[Get]]` to retrieve a value from the source object and `[[Set]]` on the destination object to
+    // assign the value
+
+        
+
+    // Note on Enumerability and ownership of properties:
+    //
+    // Every property in JS objects can be classified by three factors:
+    //
+    // > Enumerable or non-enumerable;
+    // > String or symbol;
+    // > Own property or inherited property from the prototype chain
+    //
+    // _Enumerable properties_ are those properties whose internal enumerable flag is set to true, 
+    // which is the default for properties created via simple assignment or via a property initializer. 
+    // Properties defined via `Object.defineProperty` and such are not enumerable by default. 
+    // Most iteration means e.g. `for..in` loops and `Object.keys()` only visit enumerable keys.
+    //
+    // Ownership of properties is determined by whether the property belongs to the object directly and 
+    // not to its prototype chain.
+    //
+    // 
+}
+
+
 
 
 
